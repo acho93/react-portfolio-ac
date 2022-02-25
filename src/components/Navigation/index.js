f9bb29e5ac6e4280acaa8dbf9b7100e3c3670717
Ashley Cho <ashleycho.slp@gmail.com>
2022-02-25 03:36:45 -0800

    Start portfolio section

--- src/components/Navigation/index.js
+++ src/components/Navigation/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-function Navigation (props) {
+function Navigation(props) {
     
 }
 
--- src/components/Resume/index.js
+++ src/components/Resume/index.js
@@ -1,1 +1,26 @@
 import React from 'react';
+import ResumeDoc from './ChoResume.pdf'
+
+function Resume() {
+    return (
+        <section>
+            <h2>Resume</h2>
+            <hr></hr>
+
+            <div>
+                <h3>Proficiencies</h3>
+                <ul>
+                    <li>
+                        HTML, CSS, JavaScript, MERN Stack (MongoDB, Express.js, React, Node.js), MySQL
+                    </li>
+                </ul>
+            </div>
+
+            <div>
+                <a href={ResumeDoc} Download Resume></a>
+            </div>
+        </section>
+    )
+}
+
+export default Resume;
