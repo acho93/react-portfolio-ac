2cf51f1bd80285199d3012854e1226d6527b2739
Ashley Cho <ashleycho.slp@gmail.com>
2022-02-25 05:14:05 -0800

    Add navigation

--- src/components/Navigation/index.js
+++ src/components/Navigation/index.js
@@ -1,7 +1,39 @@
 import React from 'react';
+import { NavLink } from "react-router-dom";
 
 function Navigation(props) {
-    
+    return (
+        <nav>
+            <NavLink to="/">
+                <div>
+                    <p>SOMETHING IN LEFT CORNER. MAYBE ICON?</p>
+                </div>
+            </NavLink>
+            
+            <ul className="nav">
+                <li className="nav-tab">
+                    <NavLink to="/about">
+                        <p>ABOUT</p>
+                    </NavLink>
+                </li>
+                <li className="nav-tab">
+                    <NavLink to="/portfolio">
+                        <p>PORTFOLIO</p>
+                    </NavLink>
+                </li>
+                <li className="nav-tab">
+                    <NavLink to="/contact">
+                        <p>CONTACT</p>
+                    </NavLink>
+                </li>
+                <li className="nav-tab">
+                    <NavLink to="/resume">
+                        <p>RESUME</p>
+                    </NavLink>
+                </li>
+            </ul>
+        </nav>
+    );
 }
 
 export default Navigation;
