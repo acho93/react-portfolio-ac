4d69060a0f14d740cba9902801550d845d1dd382
Ashley Cho <ashleycho.slp@gmail.com>
2022-02-24 17:01:13 -0800

    Add footer

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -1,1 +1,19 @@
 import React from 'react';
+
+function About () {
+    return (
+        <section>
+            <h2>Ashley Cho</h2>
+            <hr></hr>
+            <div>
+                <img>ADD ASHLEY'S IMAGE</img>
+
+                <p>
+                    Add a description of Ashley here!
+                </p>
+            </div>
+        </section>
+    )
+}
+
+export default About;
--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,1 +1,15 @@
 import React from 'react';
+import './Footer.css'
+
+function Footer() {
+    return (
+        <footer>
+            <h2>&copy; Ashley Cho 2022</h2><br />
+            <a href="mailto:ashleycho.slp@gmail.com"><img src="https://img.icons8.com/external-nawicon-detailed-outline-nawicon/64/000000/external-email-communication-nawicon-detailed-outline-nawicon.png" alt="Email" className="icon"/></a>
+            <a href="https://www.linkedin.com/in/ashley-cho-54518991/"><img src="https://img.icons8.com/ios/50/000000/linkedin.png" alt="LinkedIn" className="icon"/></a>
+            <a href="https://github.com/acho93"><img src="https://img.icons8.com/ios/50/000000/github--v1.png" alt="GitHub" className="icon"/></a>
+        </footer>
+    )
+}
+
+export default Footer;
